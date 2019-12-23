8c323ddc12e128adf8a1a0bff83f0c022f9533f3
Joe Fox <joefox@Joes-MBP.lan>
2019-12-23 16:06:04 +0000

    Add application config

---
+++ config/autoload/application.config.global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'http' => [
+        'base_uri' => 'https://github.com/dwyl/english-words/blob/master/',
+        'timeout'  => 10,
+    ],
+    'file' => 'words_alpha.txt',
+];
